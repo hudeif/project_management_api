1797841d47002c10e7150e6c528fe3496f0a4325
hudeifa <hudeifagit@gmail.com>
2022-05-26 10:58:20 +0300

    add properties for task,taskComment,userRole

--- src/main/java/com/example/projectManagementApi/Models/Task.java
+++ src/main/java/com/example/projectManagementApi/Models/Task.java
@@ -1,4 +1,39 @@
 package com.example.projectManagementApi.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Integer id;
+    private String title;
+    private String description;
+    private Timestamp startinDate;
+    private Timestamp endingDate;
+    private String status;
+    private String priority;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToMany(cascade = CascadeType.MERGE,fetch = FetchType.EAGER)
+    @JoinTable(name = "task_users",
+    joinColumns = @JoinColumn(name = "task_id",referencedColumnName = "task_id"),
+    inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"))
+    private List<User> users;
 }
--- src/main/java/com/example/projectManagementApi/Models/TaskComment.java
+++ src/main/java/com/example/projectManagementApi/Models/TaskComment.java
@@ -1,4 +1,25 @@
 package com.example.projectManagementApi.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TaskComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String body;
+    private Timestamp date;
 }
