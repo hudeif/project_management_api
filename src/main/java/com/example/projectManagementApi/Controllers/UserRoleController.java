9c73e848c6dfe8e56c70158e284002c3a4e2f6eb
hudeifa <hudeifagit@gmail.com>
2022-05-26 13:21:27 +0300

    finished user role controller and interface

--- src/main/java/com/example/projectManagementApi/Controllers/UserRoleController.java
+++ src/main/java/com/example/projectManagementApi/Controllers/UserRoleController.java
@@ -1,4 +1,36 @@
 package com.example.projectManagementApi.Controllers;
 
+import com.example.projectManagementApi.Interfaces.IUserRole;
+import com.example.projectManagementApi.Models.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("role")
 public class UserRoleController {
+    @Autowired
+    IUserRole iUserRole;
+
+    @GetMapping("/list")
+    public List<UserRole> getRoles(){
+        return iUserRole.findAll();
+    }
+
+    @PostMapping("/add")
+    public UserRole add(@RequestBody UserRole userRole){
+        return iUserRole.save(userRole);
+    }
+
+    @PutMapping("/update")
+    public UserRole update(@RequestBody UserRole userRole){
+        return iUserRole.save(userRole);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Integer id){
+         iUserRole.deleteById(id);
+    }
+
 }
--- src/main/java/com/example/projectManagementApi/Interfaces/IUserRole.java
+++ src/main/java/com/example/projectManagementApi/Interfaces/IUserRole.java
@@ -1,4 +1,9 @@
 package com.example.projectManagementApi.Interfaces;
 
-public interface IUserRole {
+import com.example.projectManagementApi.Models.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRole extends JpaRepository<UserRole,Integer> {
 }
