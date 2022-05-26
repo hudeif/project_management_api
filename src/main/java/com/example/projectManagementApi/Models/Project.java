44595bb00bc9fe141f1eb6f6f24884daf8591661
bilenorabdi <omar.nor@suuberp.com>
2022-05-26 11:00:33 +0300

    Added 2 Classes and their relations

--- src/main/java/com/example/projectManagementApi/Models/Project.java
+++ src/main/java/com/example/projectManagementApi/Models/Project.java
@@ -1,4 +1,37 @@
 package com.example.projectManagementApi.Models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Project {
+    @Column(name="project_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String projectName;
+    private String description;
+    private Timestamp starDate;
+    private Timestamp endDate;
+
+    @ManyToOne
+    @JoinColumn(name="task_id")
+    private List<Task> Tasks;
+
+    @ManyToMany(mappedBy = "projects")
+    private List<User> users;
+
+
 }
