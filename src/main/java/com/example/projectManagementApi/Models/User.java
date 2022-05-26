6133edf4175f9144c70d07e4fe4a2fb04aea4807
hudeifa <hudeifagit@gmail.com>
2022-05-26 11:34:45 +0300

    added object to test

--- src/main/java/com/example/projectManagementApi/Models/User.java
+++ src/main/java/com/example/projectManagementApi/Models/User.java
@@ -1,38 +1,11 @@
 package com.example.projectManagementApi.Models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
-import javax.persistence.*;
-import java.util.Set;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "user_id")
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
+    @ManyToMany(mappedBy = "users")
+    private List<Task> tasks;
 
- @ManyToOne
-    @JoinColumn(name="role_id")
-    private UserRole userRole;
-
-    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
-    @JoinTable(name = "project_Users", joinColumns = @JoinColumn(name = "project_id",referencedColumnName = "project_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id",referencedColumnName = "user_id"))
-    private Set<Project> projects;
+    private Object obj;
 }
-
-
-
-
