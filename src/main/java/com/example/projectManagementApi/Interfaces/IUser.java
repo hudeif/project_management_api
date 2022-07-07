e0fd63557fdcc3db0004387cb28ea09ff1c25d0d
hudeifa <hudeifagit@gmail.com>
2022-07-07 14:31:17 +0300

    solved bug in projectContorller

--- src/main/java/com/example/projectManagementApi/Interfaces/IUser.java
+++ src/main/java/com/example/projectManagementApi/Interfaces/IUser.java
@@ -2,8 +2,12 @@ package com.example.projectManagementApi.Interfaces;
 
 import com.example.projectManagementApi.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUser extends JpaRepository<User,Integer> {
+
+    @Query(value = "SELECT * from user u where u.email=?1 and u.password=?2 ",nativeQuery = true)
+    User checkUser(String email,String password);
 }
